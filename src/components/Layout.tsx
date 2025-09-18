0d002c0d2da779bea87a6f417bef18382ac7f436
Nidhii-ajeru <nidhiajeru3124@gmail.com>
2025-09-19 04:07:14 +0530

    remove emergency button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -35,13 +35,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
             {/* User Actions */}
             <div className="flex items-center space-x-3">
-              {/* Emergency Button */}
-              <button className="btn-danger btn-sm hidden sm:inline-flex">
-                <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L4.082 16.5c-.77.833.192 2.5 1.732 2.5z" />
-                </svg>
-                Emergency
-              </button>
               
               {/* Language Selector */}
               <select className="form-select text-sm py-1.5 px-3 min-w-0 w-auto">
